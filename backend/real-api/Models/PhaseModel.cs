98dc48e32c74dfab8f9bc181501afb98d804f6fa
Rachel Basinski <rachelbasinski@gmail.com>
2024-10-10 11:43:54 -0400

    started making models

---
+++ backend/real-api/Models/PhaseModel.cs
@@ -0,0 +1,5 @@
+public class Phase {
+    public string StartDate {get; set;}
+    public string EndDate {get; set;}
+    public string Name {get; set;}
+}
--- backend/real-api/Models/UserModel.cs
+++ backend/real-api/Models/UserModel.cs
@@ -2,4 +2,6 @@ public class User {
     public int Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public string SecturityQuestion { get; set;}
+    public string SecurityAnswer {get; set;}
 }
