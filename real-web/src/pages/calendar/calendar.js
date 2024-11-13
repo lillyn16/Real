236e54769dca62c5f9eacfa30595603d8cd1850c
Elizabeth <enbridges@eagles.usi.edu>
2024-11-13 16:28:08 -0600

    Set up calender page on React

--- real-web/src/pages/calendar/calendar.js
+++ real-web/src/pages/calendar/calendar.js
@@ -4,7 +4,92 @@ import WeeklyCalendarView from '../../components/week-calendar-view/week-calenda
 const CalendarPage = () => {
   return (
     <div className="calendar-container">
-      <WeeklyCalendarView /> 
+      <div className="Rows">
+        <button><i class="fa-solid fa-chevron-left"></i></button>
+        <div>
+          <WeeklyCalendarView /> 
+          <table>
+            <tr className="logrow">
+              <td className="logday">
+                <p>
+                  <h4>8 Days <br />Until Period</h4>
+                  <button className="log">Log Period</button>
+                </p>
+                <p>
+                  <h4>Symptoms</h4>
+                  cramp<br />
+                  acne<br />
+                  bloating
+                </p>
+                <p>
+                  <a class="log" href="/symptoms">Log Symptoms</a>
+                </p>
+              </td>
+              <td className="logday">
+                <p>
+                  <h4>7 Days <br />Until Period</h4>
+                  <button className="log">Log Period</button>
+                </p>
+                <p>
+                  <h4>Symptoms</h4>
+                  cramp<br />
+                  acne<br />
+                  bloating
+                </p>
+                <p>
+                  <a class="log" href="/symptoms">Log Symptoms</a>
+                </p>
+              </td>
+              <td className="logday">
+                <p>
+                  <h4>6 Days <br />Until Period</h4>
+                  <button className="log">Log Period</button>
+                </p>
+                <p>
+                  <h4>Symptoms</h4>
+                  cramp<br />
+                  acne<br />
+                  bloating
+                </p>
+                <p>
+                  <a class="log" href="/symptoms">Log Symptoms</a>
+                </p>
+              </td>
+              <td className="logday">
+                <p>
+                  <h4>5 Days <br />Until Period</h4>
+                  <button className="log">Log Period</button>
+                </p>
+                <p>
+                  <h4>Symptoms</h4>
+                  cramp<br />
+                  acne<br />
+                  bloating
+                </p>
+                <p>
+                  <a class="log" href="/symptoms">Log Symptoms</a>
+                </p>
+              </td>
+              <td className="logday">
+                <p>
+                  <h4>4 Days <br />Until Period</h4>
+                </p>
+              </td>
+              <td className="logday">
+                <p>
+                  <h4>3 Days <br />Until Period</h4>
+                </p>
+              </td>
+              <td className="logday">
+                <p>
+                  <h4>2 Days <br />Until Period</h4>
+                </p>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <button><i class="fa-solid fa-chevron-right"></i></button>
+      </div>
     </div>
   );
 };
