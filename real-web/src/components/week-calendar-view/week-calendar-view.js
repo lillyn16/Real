ba3f01a34f5fd1e11ed7e46771e47656aa1840b7
Rachel Basinski <rachelbasinski@gmail.com>
2024-11-14 23:31:18 -0600

    calendar days

--- real-web/src/components/week-calendar-view/week-calendar-view.js
+++ real-web/src/components/week-calendar-view/week-calendar-view.js
@@ -8,6 +8,10 @@ const WeeklyCalendarView = () => {
   const [periodData, setPeriodData] = useState(null);
   const [isPeriodWeek, setIsPeriodWeek] = useState(false);
 
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const monthNumber = new Date().getMonth();
+  let monthName = months[monthNumber];
+
   useEffect(() => {
     const getPhaseData = async () => {
       try {
@@ -28,29 +32,36 @@ const WeeklyCalendarView = () => {
 
   const renderDays = () => {
     const today = new Date();
-    // console.log(today)
 
     const startOfWeek = new Date(today)
     startOfWeek.setDate(today.getDate() - today.getDay()); // Sunday
-    // console.log(startOfWeek)
 
     return Array.from({ length: 7 }, (_, i) => {
-      const day = new Date(startOfWeek);
-      day.setDate(startOfWeek.getDate() + i);
+        const day = new Date(startOfWeek);
+        day.setDate(startOfWeek.getDate() + i);
+        const dateNumber = day.getDate();
+        const dayOfWeek = day.toLocaleString('en-US', { weekday: 'long' });
+      
 
     const showPeriodVine = isPeriodWeek && day >= new Date(periodData?.nextPeriodStart) &&
-                            day <= new Date(periodData?.nextPeriodEnd);
+        day <= new Date(periodData?.nextPeriodEnd);
 
     const isHighlighted = day.toDateString() === today.toDateString();
 
       return (
-        <div key={i} className={`day ${isHighlighted ? 'highlight' : ''}`}>
-          {day.toDateString()}
-          <hr className='break' />
-          {showPeriodVine && ( 
-            <div className='vine-img-container'>
-                <img className="vine-img" src={periodVine} alt="vine" />
-            </div> )}
+        <div className='week-calendar-container'>
+            <div key={i} className={`day ${isHighlighted ? 'highlight' : ''}`}>
+                <div className='date-container'>
+                    <div>{dayOfWeek}</div>
+                    <div>{dateNumber}</div>
+                </div>
+                
+                <hr className='calendar-hr' />
+                {showPeriodVine && ( 
+                    <div className='vine-img-container'>
+                        <img className="vine-img" src={periodVine} alt="vine" />
+                    </div> )}
+            </div>
         </div>
       );
     });
@@ -59,7 +70,8 @@ const WeeklyCalendarView = () => {
   return (
     <div className="weekly-view-container">
       <h2>Weekly Menstrual Tracker</h2>
-      <div className="days-container">{renderDays()}</div>
+      <div className='month-container'>{monthName}</div>
+      <div className='days-container'>{renderDays()}</div>
     </div>
   );
 };
